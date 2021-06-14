f9f10282f54425db74f00bf8559112508ee5a3bd
Nikolai <nlv.nki@gmail.com>
2021-06-14 17:45:39 +0300

    ver 3-4

---
+++ Exercises/3-4-wrapper.js
@@ -0,0 +1,37 @@
+'use strict';
+
+const contract = (fn, ...types) => {
+  return (...args) => {
+    args.forEach((arg,index) => {
+      let typeName = types[index].name.toLowerCase();
+      if (typeof arg !== typeName) {
+        throw new TypeError('types don"t match');
+      }
+    })
+    return fn(...args)
+  };
+
+};
+
+const add = (a, b) => a + b;
+const addNumbers = contract(add, Number, Number, Number);
+// const res = addNumbers(2, 5);
+// console.dir(res, 'Output'); // Output: 7
+
+
+// const concat = (s1, s2) => s1 + s2;
+// const concatStrings = contract(concat, String, String, String);
+// const res2 = concatStrings('Hello ', 'world!');
+// console.dir(res2); // Output: Hello world!
+
+// console.log('-'.repeat(20));
+
+// const res3 = concatStrings('Hello ', 2);
+// console.log(res3); // Output: error
+
+const re4 = addNumbers(5, '5');
+console.log(re4); // Output: error
+
+module.exports = {
+  contract
+};
